7e29cab49772810841b9c3852655dba141ff2c48
Nikolay Malinin <n.malinin@idfly.ru>
2019-01-03 13:06:10 +0100

    resolve conflicts

--- lib/yabeda/datadog/worker.rb
+++ lib/yabeda/datadog/worker.rb
@@ -13,10 +13,7 @@ module Yabeda
       SLEEP_INTERVAL = 3
 
       def self.start(queue_size: QUEUE_SIZE)
-<<<<<<< HEAD
         Logger.instance.info "start worker"
-=======
->>>>>>> aa2b4c9fe9bb173ecaf46ef54c8f1491205f13f9
         instance = new(SizedQueue.new(queue_size))
         instance.spawn_threads(NUM_THREADS)
         instance
@@ -29,10 +26,7 @@ module Yabeda
       end
 
       def enqueue(action, payload)
-<<<<<<< HEAD
         logger.info "enqueue action"
-=======
->>>>>>> aa2b4c9fe9bb173ecaf46ef54c8f1491205f13f9
         queue.push([action, payload])
       end
 
@@ -64,38 +58,6 @@ module Yabeda
       attr_reader :queue, :threads, :logger
 
       def dispatch_actions
-<<<<<<< HEAD
-        logger.info "going to dispatch actions in thread #{Thread.current.object_id}, queue size #{queue.size}"
-        send = []
-        register = []
-
-        until no_acitons?
-          action_key, action_payload = dequeue_action
-          case action_key
-          when :send then send.push(action_payload)
-          when :register then register.push(action_payload)
-          end
-        end
-
-        if send.any?
-          logger.info "sending next set of metrics in thread #{Thread.current.object_id}"
-          dogstatsd = ::Datadog::Statsd.new(
-            ENV.fetch("DATADOG_AGENT_HOST", DEFAULT_AGENT_HOST),
-            ENV.fetch("DATADOG_AGENT_PORT", DEFAULT_AGENT_PORT),
-          )
-
-          dogstatsd.batch do |stats|
-            send.each do |payload|
-              metric = payload.fetch(:metric)
-              value = payload.fetch(:value)
-              tags = payload.fetch(:tags)
-
-              logger.log_request metric do
-                stats.send(metric.type, metric.name, value, tags: tags)
-              end
-            end
-          end
-=======
         grouped_actions = Hash.new { |hash, key| hash[key] = [] }
         batch_size = 0
 
@@ -111,26 +73,11 @@ module Yabeda
 
         grouped_actions.each_pair do |group_key, group_payload|
           self.class.const_get(group_key, false).call(group_payload)
->>>>>>> aa2b4c9fe9bb173ecaf46ef54c8f1491205f13f9
         end
       end
 
-<<<<<<< HEAD
-        if register.any?
-          logger.info "updating next set of metrics in thread #{Thread.current.object_id}"
-          dogapi = ::Dogapi::Client.new('b62b7b523bf06ec87743f8e29ed17569', ENV["DATADOG_APP_KEY"])
-
-          register.each do |payload|
-            metric = payload.fetch(:metric)
-            logger.log_request metric do
-              metric.update(dogapi)
-            end
-          end
-        end
-=======
       def actions_left?
         !queue.empty?
->>>>>>> aa2b4c9fe9bb173ecaf46ef54c8f1491205f13f9
       end
 
       def no_acitons?
