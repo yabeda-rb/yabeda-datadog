3c90bb68fdf64e1eff2a2e706ef26a0e1be327b9
Nikolay Malinin <n.malinin@idfly.ru>
2019-01-04 06:21:03 +0100

    fix rubocops warnings

--- lib/yabeda/datadog/config.rb
+++ lib/yabeda/datadog/config.rb
@@ -1,13 +1,16 @@
-require 'anyway'
+# frozen_string_literal: true
+
+require "anyway"
 
 module Yabeda
   module Datadog
+    # = Describes all config variables
     class Config < Anyway::Config
       env_prefix :datadog
       config_name :datadog
       attr_config :api_key,
                   :app_key,
-                  agent_host: 'localhost',
+                  agent_host: "localhost",
                   agent_port: 8125,
                   batch_size: 10,
                   queue_size: 1000,
